1d9acb73d35af4d1499c3f5c11d7e5567aab34fd
Eom <woongi9@naver.com>
2022-06-27 14:14:58 +0900

    add Posts

---
+++ src/main/java/com/woongi9/web/service/domain/posts/Posts.java
@@ -0,0 +1,37 @@
+package com.woongi9.web.service.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor  //기본 생성자 자동 추가
+@Entity
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column
+    private LocalDateTime startDate;
+
+    @Column
+    private LocalDateTime endDate;
+
+    @Builder
+    public Posts(String title, String content, LocalDateTime startDate, LocalDateTime endDate) {
+        this.title = title;
+        this.content = content;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
---
+++ src/test/java/com/woongi9/web/service/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.woongi9.web.service.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 일정저장_불러오기(){
+        //given
+        String title = "테스트 일정";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .startDate(LocalDateTime.of(2022,6,30, 17,00,00 ))
+                .endDate(LocalDateTime.of(2022,7,30, 12,00,00 ))
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
