467b35582ca6f4b96f7c4a5962e24fe7ed8d9e0e
Eom <woongi9@naver.com>
2022-06-27 10:40:18 +0900

    Initial commit

---
+++ src/main/java/com/woongi9/web/service/WebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.woongi9.web.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/woongi9/web/service/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woongi9.web.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
