556b83be25db26a765f786ea14839bcf7b780741
Eom <woongi9@naver.com>
2022-07-05 14:50:32 +0900

    Make OAuth, Security class

---
+++ src/main/java/com/woongi9/scheduler/config/auth/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.woongi9.scheduler.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**",
+                            "/js/**", "/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                        .userService(customOAuth2UserService);
+    }
+}
---
+++ src/main/java/com/woongi9/scheduler/domain/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.woongi9.scheduler.domain.user;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
